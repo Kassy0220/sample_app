1c75739ce20e9de1b4a7ed8dc7a234800079e292
github Kassy <github@shinbokutaro0220@gmail.com>
2022-03-21 15:38:33 +0900

    Finish user signup #19

--- Gemfile
+++ Gemfile
@@ -10,6 +10,10 @@ gem 'webpacker',  '4.0.7'
 gem 'turbolinks', '5.2.0'
 gem 'jbuilder',   '2.9.1'
 gem 'bootsnap',   '1.10.3', require: false
+gem 'mini_magick'
+gem 'image_processing'
+gem 'aws-sdk-s3', require: false
+gem 'active_storage_validations'
 
 group :development, :test do
   gem 'sqlite3', '1.4.1'
---
+++ spec/features/users_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Users", type: :feature do
+  scenario "無効な値の場合は、ユーザー登録できない" do
+    visit signup_path
+    expect {
+      fill_in 'Name', with: ""
+      fill_in 'Email', with: "user@invalid"
+      fill_in 'Password', with: "foo"
+      fill_in 'Password confirmation', with: "bar"
+      click_on 'Create my account'
+    }.to change(User, :count).by(0)
+    expect(page).to have_css '#error_explanation'
+    expect(page).to have_css '.alert'
+  end
+
+  scenario "有効な値の場合は、ユーザー登録される" do
+    visit signup_path
+    expect {
+      fill_in 'Name', with: "Example User"
+      fill_in 'Email', with: "user@example.com"
+      attach_file "Avatar", "#{Rails.root}/spec/files/avatar.png"
+      fill_in 'Password', with: "password"
+      fill_in 'Password confirmation', with: "password"
+      click_on 'Create my account'
+    }.to change(User, :count).by(1)
+    expect(page).to have_current_path(user_path(1))
+    expect(page).to have_content 'Welcome to the Sample App!'
+    expect(page).to have_selector("img[src$='avatar.png']")
+  end
+end
