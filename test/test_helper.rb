141482167bd1fd9f8a6364d5c465de36a44945ed
Kassy0220 <shinbokutaro0220@gmail.com>
2024-03-06 14:37:40 +0900

    log in after valid user registration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,5 +12,9 @@ module ActiveSupport
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
     include ApplicationHelper
+
+    def logged_in?
+      !session[:user_id].nil?
+    end
   end
 end
