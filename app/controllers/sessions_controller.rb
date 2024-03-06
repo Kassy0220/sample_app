878ef2be262031f29accba45c853c3f08d05b5bb
Kassy0220 <shinbokutaro0220@gmail.com>
2024-03-06 10:13:49 +0900

    Invalid login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,14 @@
 
 class SessionsController < ApplicationController
   def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && authenticate(params[:session][:password])
+      # ログイン完了後の処理
+    else
+      flash.now[:danger] = t('errors.messages.invalid_login')
+      render 'new', status: :unprocessable_entity
+    end
+  end
 end
