936bb2e844760fb590734122d6f4a5deee07f405
Kassy0220 <shinbokutaro0220@gmail.com>
2024-03-07 15:07:50 +0900

    User logout

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -11,6 +11,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: '', password: '' } }
+
+    assert_not logged_in?
     assert_response :unprocessable_entity
     assert_template 'sessions/new'
     assert_select '.alert-danger', 'メールアドレスとパスワードの組み合わせが正しくありません'
@@ -22,6 +24,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: 'michael@example.com', password: 'invalid' } }
+
+    assert_not logged_in?
     assert_response :unprocessable_entity
     assert_template 'sessions/new'
     assert_select '.alert-danger', 'メールアドレスとパスワードの組み合わせが正しくありません'
@@ -29,13 +33,24 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select '.alert-danger', count: 0
   end
 
-  test 'login with valid information' do
+  test 'login with valid information followed by logout' do
     post login_path, params: { session: { email: 'michael@example.com', password: 'testtest' } }
+
+    assert logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select 'a[href=?]', login_path, count: 0
     assert_select 'a[href=?]', logout_path
     assert_select 'a[href=?]', user_path(@user)
+
+    delete logout_path
+    assert_not logged_in?
+    assert_response :see_other
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
   end
 end
